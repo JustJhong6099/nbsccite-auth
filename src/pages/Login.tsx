87e8df7b8c290ec75f772067142dcf434f73ec0e
Jhong Emata <20211199@nbsc.edu.ph>
2025-08-29 03:15:52 +0000

    modified:   public/background.jpg modified:   src/pages/Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -58,7 +58,7 @@ const Login: React.FC = () => {
           <div className="inline-flex items-center justify-center w-16 h-16 bg-gradient-primary rounded-full mb-4 shadow-glow">
             <GraduationCap className="h-8 w-8 text-primary-foreground" />
           </div>
-          <h1 className="text-2xl font-bold text-foreground">NBSC-ICS</h1>
+          <h1 className="text-2xl font-bold text-foreground">NORTHER BUKIDNON STATE COLLEGE</h1>
           <p className="text-muted-foreground">Entity Extraction System</p>
         </div>
 
